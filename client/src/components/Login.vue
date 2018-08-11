80a5a974a99094ee4d0afccd69b97a32b2c8789b
shahnirav <shah.nirav@me.com>
2018-08-11 12:57:58 +0100

    commit

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,46 +1,50 @@
 <template>
-  <div class="u-mt-xlarge o-page__card o-page__card--horizontal">
-    <div class="c-card c-login-horizontal">
-      <div class="c-login__content-wrapper">
-        <header class="c-login__header">
-          <a class="c-login__icon c-login__icon--rounded c-login__icon--left" href="#!">
-            <img src="/static/img/logo-login.svg" alt="Dashboard's Logo">
-          </a>
+  <div class="login-container">
+    <div class="u-mt-xlarge o-page__card o-page__card--horizontal">
+      <div class="c-card c-login-horizontal">
+        <div class="c-login__content-wrapper">
+          <header class="c-login__header">
+            <a class="c-login__icon c-login__icon--rounded c-login__icon--left" href="#!">
+              <img src="/static/img/logo.png" alt="Dashboard's Logo">
+            </a>
 
-          <h2 class="c-login__title">Sign In</h2>
-        </header>
+            <h2 class="c-login__title">Sign In</h2>
+          </header>
 
-        <form class="c-login__content">
-          <div class="c-field u-mb-small">
-            <label class="c-field__label" for="email">Email Address</label>
-            <input class="c-input" type="email" id="email" name="email"
-                   placeholder="user@uniproc.com" v-model="user.email" v-validate="'required'">
-            <small v-show="errors.has('email')" class="c-field__message u-color-danger">
-              <i class="fa fa-times-circle"></i>{{ errors.first('email') }}
-            </small>
-          </div>
+          <form class="c-login__content">
+            <div class="c-field u-mb-small">
+              <label class="c-field__label" for="email">Email Address</label>
+              <input class="c-input" type="email" id="email" name="email"
+                     placeholder="user@uniproc.com" v-model="user.email" v-validate="'required'">
+              <small v-show="errors.has('email')" class="c-field__message u-color-danger">
+                <i class="fa fa-times-circle"></i>{{ errors.first('email') }}
+              </small>
+            </div>
 
-          <div class="c-field u-mb-small">
-            <label class="c-field__label" for="password">Password</label>
-            <input class="c-input" type="password" id="password" name="password" placeholder="password"
-                   v-model="user.password" v-validate="'required'">
-            <small v-show="errors.has('password')" class="c-field__message u-color-danger">
-              <i class="fa fa-times-circle"></i>{{ errors.first('password') }}
-            </small>
+            <div class="c-field u-mb-small">
+              <label class="c-field__label" for="password">Password</label>
+              <input class="c-input" type="password" id="password" name="password" placeholder="password"
+                     v-model="user.password" v-validate="'required'">
+              <small v-show="errors.has('password')" class="c-field__message u-color-danger">
+                <i class="fa fa-times-circle"></i>{{ errors.first('password') }}
+              </small>
 
-          </div>
+            </div>
 
-          <button v-on:click.prevent="login" class="c-btn c-btn--success c-btn--fullwidth" type="submit">Sign in
-          </button>
-        </form>
-      </div>
+            <button v-on:click.prevent="login" class="c-btn c-btn--success c-btn--fullwidth" type="submit">Sign in
+            </button>
+          </form>
+        </div>
 
-      <div class="c-login__content-image">
-        <img src="/static/img/login2.jpg" alt="Welcome to Dashboard UI Kit">
+        <div class="c-login__content-image">
+          <img src="/static/img/login2.png" alt="Welcome to Uniprocure">
 
-        <h3>Welcome to Workspace</h3>
-        <p class="u-text-large">
-          An interface designed for freelancers to help them manage their work and clients. Created with Dashboard UI Kit.</p>
+          <!--
+                    <h3>Welcome to Uniprocure</h3>
+                    <p class="u-text-large">
+                      GS1 compliant Inventory & Catalogue Management System for NHS Trusts.</p>
+          -->
+        </div>
       </div>
     </div>
   </div>
@@ -79,4 +83,25 @@
     }
   }
 </script>
-<style></style>
+<style>
+  .c-login-horizontal .c-login__icon {
+    background: none;
+  }
+
+  .c-login-horizontal .c-login__content-image img {
+    opacity: 1;
+  }
+
+  .c-login-horizontal .c-login__content-image {
+    background-color: #ffffff;
+  }
+
+  .login-container {
+    height: 100vh;
+    background-image: url("/static/img/login-bg-1.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    padding-top: 60px;
+  }
+</style>
