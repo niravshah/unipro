12e47a5a3c8fef2fbda60798a11839f0828cd552
shahnirav <shah.nirav@me.com>
2018-09-16 23:25:58 +0100

    commit

--- server/app.js
+++ server/app.js
@@ -58,56 +58,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
-app.use(function (req, res, next) {
-
-    var sdomain = null;
-
-    if (req.subdomains[0]) {
-        sdomain = req.subdomains[0];
-    } else if (typeof req.body.subdomain !== 'undefined') {
-        sdomain = req.body.subdomain;
-    } else if (typeof req.query.subdomain !== 'undefined') {
-        sdomain = req.query.subdomain;
-    }
-    if (sdomain !== null) {
-
-        if (Sequelize.tenants) {
-            var found = false;
-            Sequelize.tenants.forEach(tenant => {
-                if (tenant.sudomain === sdomain) {
-                    if (!req.body.data) {
-                        req.body.data = []
-                    }
-                    req.body.data['tenant'] = tenant.id;
-                    found = true;
-                    next();
-                }
-            });
-
-            if (!found) {
-                next(new Error('No valid tenants founds'))
-            }
-
-        } else {
-            Sequelize.Tenant.findOne({where: {sudomain: sdomain}}).then(tenant => {
-                if (tenant != null) {
-                    if (!req.body.data) {
-                        req.body.data = []
-                    }
-                    req.body.data['tenant'] = tenant.id;
-                    next();
-                } else {
-                    next(new Error('No valid tenants founds'))
-                }
-            }).catch(err => {
-                next(err)
-            });
-        }
-
-    } else {
-        next(new Error('No valid tenants founds'))
-    }
-});
+var tenantFinder = require('./middleware/tenantFinder');
+app.use(tenantFinder);
 
 app.use('/', index);
 app.use('/api/auth', auth);
---
+++ server/middleware/tenantFinder.js
@@ -0,0 +1,51 @@
+var Sequelize = require('../sqlize/models');
+module.exports = function (req, res, next) {
+
+    var sdomain = null;
+
+    if (req.subdomains[0]) {
+        sdomain = req.subdomains[0];
+    } else if (typeof req.body.subdomain !== 'undefined') {
+        sdomain = req.body.subdomain;
+    } else if (typeof req.query.subdomain !== 'undefined') {
+        sdomain = req.query.subdomain;
+    }
+    if (sdomain !== null) {
+
+        if (Sequelize.tenants) {
+            var found = false;
+            Sequelize.tenants.forEach(tenant => {
+                if (tenant.sudomain === sdomain) {
+                    if (!req.body.data) {
+                        req.body.data = []
+                    }
+                    req.body.data['tenant'] = tenant.id;
+                    found = true;
+                    next();
+                }
+            });
+
+            if (!found) {
+                next(new Error('No valid tenants founds'))
+            }
+
+        } else {
+            Sequelize.Tenant.findOne({where: {sudomain: sdomain}}).then(tenant => {
+                if (tenant != null) {
+                    if (!req.body.data) {
+                        req.body.data = []
+                    }
+                    req.body.data['tenant'] = tenant.id;
+                    next();
+                } else {
+                    next(new Error('No valid tenants founds'))
+                }
+            }).catch(err => {
+                next(err)
+            });
+        }
+
+    } else {
+        next(new Error('No valid tenants founds'))
+    }
+};
